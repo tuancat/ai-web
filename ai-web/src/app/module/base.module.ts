cbb6268740897275f722c8c521d726ff5961edab
Tuan Cat <tuancnh@ecoe.vn>
2020-02-05 11:24:33 +0700

    + add lib

---
+++ ai-web/src/app/module/base.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {NgbAlertModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+imports: [
+  NgbModule,
+  NgbAlertModule
+],
+})
+export class BaseModule {
+
+}
